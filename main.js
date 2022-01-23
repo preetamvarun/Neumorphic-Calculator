a206c3732772ffb5628b44e0a933f6871f507fec
Preetam Varun <preetamarun99@gmail.com>
2022-01-23 15:59:14 +0530

    still have to deal with % operator

--- main.js
+++ main.js
@@ -10,14 +10,7 @@ const history = document.getElementById('calciBox');
 const icon = togglerBox.firstElementChild;
 let historyValue = document.getElementById('history-value');
 let outputValue = document.getElementById('output-value');
-
-function getHistoryValue(){
-    return historyValue.textContent;
-}
-
-function getOutputValue(){
-    return outputValue.textContent;
-}
+let operatorsArr = ["+","-","*","/"];
 
 function clearHistoryValue(){
     historyValue.innerHTML = "";
@@ -27,44 +20,117 @@ function clearOutputValue(){
     outputValue.innerHTML = "";
 }
 
-/* 0 . 9 */
-for(let i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener('click',function(){
-        historyValue.innerHTML += numbers[i].textContent;
+function calculatePercentage(str ,num = 1){
+    number = Number(str);
+    return (number/100) * num;
+}
+
+function getOutputValue(){
+    return eval(historyValue.innerHTML);
+}
+
+function setTimeOut(){
+    setTimeout(function(){
+        historyValue.innerHTML = "";   
+    },2000);
+}
+
+function calci(x){
+
+    outputValue.classList.remove('error');
+    historyValue.classList.remove('error');
+
+    // dealing with + - / *
+    if(operatorsArr.indexOf(x.textContent) !== -1){
+        historyValue.innerHTML += x.textContent;
+        return;
+    }
+
+    // dealing with =
+    else if(x.textContent === "="){
+        clearOutputValue();
         try{
-            outputValue.innerHTML = eval(historyValue.innerHTML);  
-        }
+            isNaN(getOutputValue()) ? historyValue.innerHTML = "Can't perform"
+            : historyValue.innerHTML = getOutputValue();
+            if(historyValue.textContent === "Can't perform" || historyValue.textContent === 'Infinity'){
+                historyValue.classList.add('error');
+                setTimeOut();
+            }
+        } 
         catch(err){
+            historyValue.textContent = 'Format Error!';
+            historyValue.classList.add('error');   
+            setTimeOut();
+        }
+    }
+
+    // dealing with %
+    else if(x.textContent === "%"){
+
+    }
+
+    // dealing with numbers 
+    else{
+        historyValue.innerHTML += x.textContent;
+        try{
+            if(isNaN(getOutputValue())){
+                outputValue.innerHTML = "Can't perform";
+                outputValue.classList.add('error');
+            } else{
+                outputValue.innerHTML = getOutputValue();
+            }
+        } catch(err){
             outputValue.textContent = "Format Error!";
             outputValue.classList.add('error');
         }
+    }
+}
+
+
+/* 0 . 9 */
+for(let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener('click',function(){
+        // if (resultArr.indexOf(historyValue.innerHTML) !== -1) calci(numbers[i]);
+        if(historyValue.innerHTML !== 'Format Error!'
+        && historyValue.innerHTML !== "Can't perform" && historyValue.innerHTML != 'Infinity'
+        ) calci(numbers[i]);
     });
 }
 
-/* + - / x = */
+/* + - / x =  %*/
 for(let i = 0; i < operators.length; i++){
     operators[i].addEventListener('click', function(){
-        if(operators[i].textContent === "="){
-            historyValue.innerHTML = getOutputValue();
-            clearOutputValue();
-        } else{
-            historyValue.innerHTML += operators[i].textContent;
-        }
-    })
+        if(historyValue.innerHTML !== 'Format Error!'
+        && historyValue.innerHTML !== "Can't perform" && historyValue.innerHTML != 'Infinity'
+        ) calci(operators[i]);
+    });
 }
 
 /*AC DEL*/
 for(let i = 0; i < clearItems.length; i++){
     clearItems[i].addEventListener('click',function(){
+
         if(clearItems[i].textContent === 'Del'){
-            let str = getHistoryValue();
-            str = str.substring(0,str.length-1);
-            historyValue.innerHTML = str;
-        } else{
+
+            clearOutputValue();
+
+            let str = historyValue.innerHTML;
+
+            if(str === 'Infinity' || str === "Can't perform" || str === 'Format Error!'){
+                clearHistoryValue();
+            }
+
+            else{
+                str = str.substring(0,str.length-1);
+                historyValue.innerHTML = str;
+            }
+        } 
+        
+        else{
             clearHistoryValue();
             clearOutputValue();
         }       
-    })
+    });
 }
 
 /* TOGGLE THEMES */
